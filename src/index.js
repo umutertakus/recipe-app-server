49b8c5905a9fd44fe7be99e7d1d0078d0f26e966
umutertakus <umutertakus@hotmail.com>
2023-03-14 16:44:40 +0300

    Create index file

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+const { MONGO_USERNAME, MONGO_PASSWORD } = process.env;
+
+mongoose.connect(`mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@recipes.g8yqers.mongodb.net/recipes?retryWrites=true&w=majority`)
+
+app.listen(3300, () => console.log("Server started."))
