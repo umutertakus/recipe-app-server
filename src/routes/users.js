be2265edbf080313fbf8474639b30bb8186b66ef
umutertakus <umutertakus@hotmail.com>
2023-03-16 02:17:12 +0300

    Add login endpoint

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,9 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { UserModel } from "../models/Users.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const router = express.Router();
 
@@ -20,6 +23,25 @@ router.post("/register", async (req, res) => {
   await newUser.save();
 
   res.json({ message: "User registered successfully." });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await UserModel.findOne({ username });
+
+  if (!user) {
+    return res.json({ message: "User doesn't exists." });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.json({ message: "Username or password is incorrect." });
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+  res.json({ token, userId: user._id });
 });
 
 export { router as userRouter };
