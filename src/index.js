409081b267933cacd37a42076ba5f2acdd4db3a0
umutertakus <umutertakus@hotmail.com>
2023-03-15 14:17:07 +0300

    Create users route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import { userRouter } from "./routes/users.js";
+
 const app = express();
 
 app.use(express.json());
@@ -11,6 +13,8 @@ app.use(cors());
 dotenv.config();
 
 const { MONGO_USERNAME, MONGO_PASSWORD } = process.env;
+
+app.use("/auth", userRouter);
 
 mongoose.connect(
   `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@recipes.g8yqers.mongodb.net/recipes?retryWrites=true&w=majority`
---
+++ src/routes/users.js
@@ -0,0 +1,7 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const router = express.Router();
+
+export { router as userRouter };
